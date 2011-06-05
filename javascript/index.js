f654bd9ec6fab3f9f0a8ba2daff5439a5b59dc0f
liberjose <liberjose@liberjose-Aspire-E360.(none)>
2011-06-05 18:41:52 +0100

    Funcion map que indexa correctamente

--- javascript/index.js
+++ javascript/index.js
@@ -1,18 +1,22 @@
 doc={
-name:"Jose"
+name:"ComeCastañas"
 };
 
-var map=function(doc){
+function map (doc){
 if(doc.name){
-  len=doc.name.length;
+  var len=doc.name.length;
+  var n=0;
+  var key='';
   console.log(len);
+  results={}
   for (var i=0;i<len;i++){
     for (var j=0;j<(len-i);j++){
-    
-    console.log(doc.name.substring(j,j+i+1)); 
+      n++;
+      key=doc.name.substring(j,j+i+1);
+      results[key]=doc.name;
     }
-
   }
+  console.dir(results);
 }
 
 };
