416e973e8d0a971cd4d531dae27dd214827ce222
Jose <jose@jose-900.(none)>
2011-06-08 18:27:47 +0100

    Cambios para poder mergear

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,15 @@
 doc={
-name:"Jose"
+nombre:"Jose"
 };
 
 var map=function(doc){
-if(doc.name){
-  len=doc.name.length;
+if(doc.nombre){
+  len=doc.nombre.length;
   console.log(len);
   for (var i=0;i<len;i++){
     for (var j=0;j<(len-i);j++){
     
-    console.log(doc.name.substring(j,j+i+1)); 
+    console.log(doc.nombre.substring(j,j+i+1)); 
     }
 
   }
