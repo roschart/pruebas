edc0598c4303299f4699a6ea7224d5e77ce6e484
unknown <joseluis.dominguez@DST-01-JLD.(none)>
2012-03-19 14:53:44 +0100

    Se crea otra carpeta para probar knockout en vez de jquery a pelo

---
+++ workflow/prototipos/002_simple_workflow_knockout-2.0.0/workflow.js
@@ -0,0 +1,48 @@
+
+(function ($) {
+    var methods = {
+        init: function (workflow) {
+            var $this = $(this);
+            $this.on("click", ".task_actions", function (event) {
+                methods.taskDone($(this).parent());
+            });
+            return this.each(function () {
+                $("#stageTemplate").tmpl(workflow.stages).appendTo($this);
+                methods.addNewTaskButton();
+            
+            });
+        },
+        addNewTaskButton:function(){
+            var $botonPlus=$('<span class="button">+</span>').button().on('click',function(){alert("añadir")});
+                $(".stage:first").find('.stage_head').append($botonPlus);
+
+        },
+
+        taskDone: function ($task) {
+            //Chech if is a real task 
+            if (!$task.hasClass('task')) {
+                alert("Error de programación: No se a utilizado un $task válido para el método taskDone");
+            }
+            var $nextStage = $task.closest(".stage").next();
+
+            if ($nextStage.length) {
+                $nextStage.append($task);
+            } else {
+                $task.remove();
+            };
+        }
+    };
+
+    $.fn.workFlow = function (method) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery.tooltip');
+        }
+
+    };
+
+
+})(jQuery);
