40816fe29d242396eff7b6285d4362fda348a1ac
unknown <joseluis.dominguez@DST-01-JLD.(none)>
2012-04-04 14:57:40 +0200

    Se utiliza un boton de jquery y se marca la tarea como realizada

--- workflow/prototipos/002_simple_workflow_knockout-2.0.0/workflow.js
+++ workflow/prototipos/002_simple_workflow_knockout-2.0.0/workflow.js
@@ -1,17 +1,37 @@
-function Task(id, title, done){
+
+ko.bindingHandlers.jqButton = {
+    init: function(element,valueAccessor) {
+		  var currentValue = valueAccessor();
+		  $(element).button({disabled:currentValue.disabled});
+    },
+    update: function(element,valueAccessor) {
+		  var currentValue = valueAccessor();
+		  $(element).button({disabled:currentValue.disabled});
+    },
+};
+
+
+function Task(id, title, isDone){
 	var self=this;
 	self.id=id;
 	self.title=title;
-	self.done=done||false;
+	self.isDone=ko.observable(isDone);
+	self.done=function(){self.isDone(true);
+	}
 
 }
 
 function WorkFlowViewModel(){
 	var self=this;
 	self.tasks=ko.observableArray([
-		new Task(1,"Prueba",false),
-		new Task(2,"Otra",true)		
+		new Task(1,"Pendiente",false),
+		new Task(2,"Realizada",true),
+		new Task(3,"Otra mas pendiente",false)
+		
 		]);
 }
 
 ko.applyBindings(new WorkFlowViewModel);
+
+
+
