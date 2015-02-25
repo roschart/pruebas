f22754d578fdd6aafcb7ad7dc6bbe472b68661b4
Jose <jlbalirac@hotmail.com>
2015-02-25 18:24:50 +0100

    Se elimina thisAmount [ReplaceTemp with Query]

--- refactoringExercices/videoStore/src/balirac/videoStore/Customer.java
+++ refactoringExercices/videoStore/src/balirac/videoStore/Customer.java
@@ -24,15 +24,14 @@ public class Customer {
 		int frequentRenterPoints = 0;
 		String result = "Rental Record for " + getName() + "\n";
 		for (Rental each : _rentals) {
-			double thisAmount = each.getCharge();
 			// add frequent renter points
 			frequentRenterPoints ++;
 			// add bonus for a two day new release rental
 			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
 				frequentRenterPoints ++;
 			//show figures
-			result += "\t" + each.getMovie().getTitle()+ "\t" +	String.valueOf(thisAmount)+ "\n";
-			totalAmount +=	thisAmount;		
+			result += "\t" + each.getMovie().getTitle()+ "\t" +	String.valueOf(each.getCharge())+ "\n";
+			totalAmount +=	each.getCharge();		
 
 		}
 		//add footer lines
