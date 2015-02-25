0738754b2595719bc8ee44d394f4f1bc3a356630
Jose <jlbalirac@hotmail.com>
2015-02-25 18:09:46 +0100

    Se mueve amountFor a Rental y se renombra

--- refactoringExercices/videoStore/src/balirac/videoStore/Customer.java
+++ refactoringExercices/videoStore/src/balirac/videoStore/Customer.java
@@ -24,7 +24,7 @@ public class Customer {
 		int frequentRenterPoints = 0;
 		String result = "Rental Record for " + getName() + "\n";
 		for (Rental each : _rentals) {
-			double thisAmount = amountFor(each);
+			double thisAmount = each.getCharge();
 			// add frequent renter points
 			frequentRenterPoints ++;
 			// add bonus for a two day new release rental
@@ -41,25 +41,5 @@ public class Customer {
 
 		return result;
 
-	}
-
-	private double amountFor(Rental each) {
-		double thisAmount=0;
-		switch (each.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			thisAmount += 2;
-			if (each.getDaysRented() > 2)
-				thisAmount += (each.getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			thisAmount += each.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (each.getDaysRented() > 3)
-				thisAmount += (each.getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return thisAmount;
 	}
 }
