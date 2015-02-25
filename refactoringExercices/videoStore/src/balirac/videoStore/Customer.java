400ac10c5d8a9a56def5f21fe7c6b1cb137a8121
Jose <jlbalirac@hotmail.com>
2015-02-25 17:47:09 +0100

    Se extrae el metodo que calcula el coste

--- refactoringExercices/videoStore/src/balirac/videoStore/Customer.java
+++ refactoringExercices/videoStore/src/balirac/videoStore/Customer.java
@@ -24,23 +24,7 @@ public class Customer {
 		int frequentRenterPoints = 0;
 		String result = "Rental Record for " + getName() + "\n";
 		for (Rental each : _rentals) {
-			double thisAmount = 0;
-			// determine amounts for each line
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				break;
-			}
+			double thisAmount = amountFor(each);
 			// add frequent renter points
 			frequentRenterPoints ++;
 			// add bonus for a two day new release rental
@@ -57,5 +41,25 @@ public class Customer {
 
 		return result;
 
+	}
+
+	private double amountFor(Rental each) {
+		double thisAmount=0;
+		switch (each.getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			thisAmount += 2;
+			if (each.getDaysRented() > 2)
+				thisAmount += (each.getDaysRented() - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			thisAmount += each.getDaysRented() * 3;
+			break;
+		case Movie.CHILDRENS:
+			thisAmount += 1.5;
+			if (each.getDaysRented() > 3)
+				thisAmount += (each.getDaysRented() - 3) * 1.5;
+			break;
+		}
+		return thisAmount;
 	}
 }
