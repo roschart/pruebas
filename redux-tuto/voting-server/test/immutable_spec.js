ed3514b1511b5eba9d9f6013766bb2be057e1561
jlbalirac <jlbalirac@hotmail.com>
2015-09-12 19:35:55 +0200

    add a fail test

--- redux-tuto/voting-server/test/immutable_spec.js
+++ redux-tuto/voting-server/test/immutable_spec.js
@@ -1,4 +1,5 @@
 import {expect} from 'chai';
+import {List} from 'immutable';
 
 describe('immutability', () => {
 
@@ -15,7 +16,18 @@ describe('immutability', () => {
       expect(nextState).to.equal(43);
       expect(state).to.equal(42);
     });
-
   });
+
+	describe('A list',()=>{
+			function addMovie(list,movie){
+				return list.push(movie);
+			}	
+			it('add a movie to list',()=>{
+				let state = List.of('Hello','World');
+				let newState=addMovie('Cruel');
+				expect(state).to.equal(List.of('Hello','World'));
+				expect(newState).to.equal(List.of('Hello','World','cruel'));
+				})
+			});
 
 });
