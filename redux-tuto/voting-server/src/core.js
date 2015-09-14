34c600d6e2d6f3f9435535c034441e5c0a2fad69
Jose Luis Domínguez <jl.balirac@payvision.com>
2015-09-14 11:04:13 +0200

    Add setEntries logic and test

---
+++ redux-tuto/voting-server/src/core.js
@@ -0,0 +1,4 @@
+export function setEntries(state,entries){
+  return state.set('entries',entries);
+}
+
--- redux-tuto/voting-server/test/immutable_spec.js
+++ redux-tuto/voting-server/test/immutable_spec.js
@@ -1,6 +1,5 @@
 import {expect} from 'chai';
-import {List} from 'immutable';
-
+import {List, Map} from 'immutable';
 describe('immutability', () => {
 
   describe('a number', () => {
@@ -8,7 +7,6 @@ describe('immutability', () => {
     function increment(currentState) {
       return currentState + 1;
     }
-
     it('is immutable', () => {
       let state = 42;
       let nextState = increment(state);
@@ -18,16 +16,39 @@ describe('immutability', () => {
     });
   });
 
-	describe('A list',()=>{
-			function addMovie(list,movie){
-				return list.push(movie);
-			}	
-			it('add a movie to list',()=>{
-				let state = List.of('Hello','World');
-				let newState=addMovie(state,'Cruel');
-				expect(state).to.equal(List.of('Hello','World'));
-				expect(newState).to.equal(List.of('Hello','World','Cruel'));
-				})
-			});
-
+  describe('A list', () => {
+    function addItemToList(list, text) {
+      return list.push(text);
+    }
+    it('add a item to list', () => {
+      let state = List.of('Hello', 'World');
+      let newState = addItemToList(state, 'Cruel');
+      expect(state).to.equal(List.of('Hello', 'World'));
+      expect(newState).to.equal(List.of('Hello', 'World', 'Cruel'));
+    });
+  });
+  describe('a Tree', () => {
+    function addTextToComplexTree(tree, text) {
+      return tree.update('movies', movies=>movies.push(text));
+    }
+    it('is immutable', () => {
+      let state = Map({
+        movies: List.of('Trainspotting', '28 Days Later')
+      });
+      let nextState = addTextToComplexTree(state, 'Sunshine');
+      expect(nextState).to.equal(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later',
+          'Sunshine'
+        )
+      }));
+      expect(state).to.equal(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later'
+        )
+      }));
+    });
+  });
 });
