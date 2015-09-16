fdc645a1cf1b31ee52034f00181e10715c514a67
Jose Luis Domínguez <jl.balirac@payvision.com>
2015-09-16 09:49:48 +0200

    Add vote funtion

--- redux-tuto/voting-server/src/core.js
+++ redux-tuto/voting-server/src/core.js
@@ -1,4 +1,29 @@
-export function setEntries(state,entries){
-  return state.set('entries',entries);
+import {
+  List, Map
+}
+from 'immutable';
+
+export function setEntries(state, entries) {
+  return state.set('entries', List(entries));
 }
 
+export function next(state) {
+  return state.merge({
+    vote: Map({
+      pair: toVote(state)
+    }),
+    entries: restEntries(state)
+  });
+}
+
+export function vote(state, movie) {
+  return state.updateIn(['vote', 'tally', movie], 0 ,(vote)=>vote+1);
+}
+
+function toVote(state) {
+  return state.get('entries').take(2);
+}
+
+function restEntries(state) {
+  return state.get('entries').skip(2);
+}
--- redux-tuto/voting-server/test/core_spec.js
+++ redux-tuto/voting-server/test/core_spec.js
@@ -1,7 +1,15 @@
-import {List, Map} from 'immutable';
-import {expect} from 'chai';
-
-import {setEntries} from '../src/core';
+import {
+  List, Map, fromJS
+}
+from 'immutable';
+import {
+  expect
+}
+from 'chai';
+import {
+  setEntries, next, vote
+}
+from '../src/core';
 describe('Application logic', () => {
   describe('setEntries', () => {
     it('adds the entries to the state', () => {
@@ -12,12 +20,43 @@ describe('Application logic', () => {
         entries: List.of('Trainspotting', '28 Days Later')
       }));
     });
-    it('converts to immutable',()=>{
-      const initState=Map();
-      const entries =['Trainspotting', '28 Days Later'];
-      const nextState=setEntries(initState,entries);
+    it('converts to immutable', () => {
+      const initState = Map();
+      const entries = ['Trainspotting', '28 Days Later'];
+      const nextState = setEntries(initState, entries);
       expect(nextState).to.equal(Map({
         entries: List.of('Trainspotting', '28 Days Later')
+      }));
+    });
+  });
+  describe('next', () => {
+    it('takes the next two entries under vote', () => {
+      const state = fromJS({
+        entries: ['Trainspotting', '28 Days Later', 'Sunshine']
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(fromJS({
+        vote: {
+          pair: ['Trainspotting', '28 Days Later']
+        },
+        entries: ['Sunshine']
+      }));
+    });
+  });
+  describe('vote', () => {
+    it('creates a tally for the voted entry', () => {
+      const state = fromJS({
+        vote: {
+          pair: ['Trainspotting', '28 Days Later']
+        }
+      });
+      const nextState=vote(state,'Trainspotting');
+      console.log(nextState.toJSON());
+      expect(nextState).to.equal(fromJS({
+        vote: {
+          pair: ['Trainspotting', '28 Days Later'],
+          tally: {'Trainspotting':1}
+        }
       }));
     });
   });
