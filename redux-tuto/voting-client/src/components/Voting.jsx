f1723148854f44bb2d6622a55e878ff99f659b2d
Roschart <jlbalirac@hotmail.com>
2015-09-23 15:42:21 +0200

    Client with hot react, webpack, hotload, and simple component

---
+++ redux-tuto/voting-client/src/components/Voting.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default React.createClass({
+  getPair(){
+    return this.props.pair || [];
+  },
+   render(){
+    return <div className="voting">
+      {this.getPair().map(entry =>
+        <button key={entry}>
+          <h1>{entry}</h1>
+        </button>
+      )}
+    </div>;
+  }
+});
---
+++ redux-tuto/voting-client/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Voting from './components/Voting';
+const pair = ['Trainspotting', '28 Days Later'];
+
+React.render(
+  <Voting pair={pair} />,
+  document.getElementById('app')
+);
