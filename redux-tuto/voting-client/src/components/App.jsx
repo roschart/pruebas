c42f869524891468d3d85636dffc36a350bea018
Roschart <jlbalirac@hotmail.com>
2015-09-24 11:24:03 +0200

    Fix route and add logic to results

--- redux-tuto/voting-client/src/components/App.jsx
+++ redux-tuto/voting-client/src/components/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import {RouteHandler} from 'react-router';
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
+import Voting from './Voting';
+import {Link } from 'react-router';
 
 const pair = List.of('Trainspotting', '28 Days Later');
+const tally = Map({'Trainspotting': 5, '28 Days Later': 4});
 
 export default React.createClass({
   render: function() {
-    return (<RouteHandler pair={pair} />);
+    return (
+      <div>
+        <Voting pair={pair} tally={tally}/>
+        <Link to='results'>results</Link>
+      </div>
+    );
   }
 });
--- redux-tuto/voting-client/src/index.jsx
+++ redux-tuto/voting-client/src/index.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import Router, {
-  Route,
-  DefaultRoute
-} from 'react-router';
+import {Router, Route} from 'react-router';
 import App from './components/App';
-import Voting from './components/Voting';
+//import Voting from './components/Voting';
 import Results from './components/Results';
 
-const routes = <Route handler={App}>
-    <Route handler={Results} path='results'/>
-    <DefaultRoute handler={Voting}/>
-  </Route>;
+const routes = (
+  <Router>
+    <Route component={App} path='/'/>
+    <Route component={Results} path='results' />
+  </Router>
+);
 
-Router.run(routes, (Root) => {
-  React.render(<Root/>, document.getElementById('app'));
-});
+React.render(routes,document.getElementById('app'));
