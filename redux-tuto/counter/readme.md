0a6d097eaaf8c7f38fa2d7e33ec1bfe1814cccdb
Roschart <jlbalirac@hotmail.com>
2015-09-25 14:21:10 +0200

    Create actions

---
+++ redux-tuto/counter/readme.md
@@ -0,0 +1,2 @@
+# Readme
+The example made https://github.com/rackt/redux/tree/master/examples/counter on my way ;)
--- redux-tuto/counter/src/index.jsx
+++ redux-tuto/counter/src/index.jsx
@@ -1,1 +1,33 @@
-console.log('I m alive');
+import {createStore} from 'redux';
+import React from 'react';
+import Counter from './components/Counter';
+import * as CounterActions from './actions/counter';
+
+console.log(CounterActions);
+
+function counter(state = 0, action = null) {
+  switch (action.type) {
+  case CounterActions.INCREMENT_COUNTER :
+    return state + 1;
+  case CounterActions.DECREMENT_COUNTER :
+    return state - 1;
+  }
+  return state;
+}
+
+let store = createStore(counter);
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch({
+  type: CounterActions.INCREMENT_COUNTER
+});
+// 1
+store.dispatch({
+  type: CounterActions.INCREMENT_COUNTER
+});
+// 2
+store.dispatch({
+  type: CounterActions.DECREMENT_COUNTER
+});
+
+React.render(<Counter {...CounterActions} counter={0} />, document.getElementById('app'));
