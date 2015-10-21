0c8e7425021e8c17d30b5901f8ba2409ac6e5149
Roschart <jlbalirac@hotmail.com>
2015-10-21 17:23:50 +0200

    Ramda version copy of most adequete guide

---
+++ katas/flickr/ramda/index.js
@@ -0,0 +1,35 @@
+function url(t) {
+  return 'https://api.flickr.com/services/feeds/photos_public.gne?tags=' + t + '&format=json&jsoncallback=?';
+}
+
+
+var img = function(url) {
+  return $('<img />', {
+    src: url
+  });
+};
+
+var mediaUrl = R.compose(R.prop('m'), R.prop('media'));
+var srcs = R.compose(R.map(mediaUrl), R.prop('items'));
+var images = R.compose(R.map(img), srcs);
+
+
+var Impure = {
+  getJSON: R.curry(function(callback, url) {
+    $.getJSON(url).done(callback);
+  }),
+
+  setHtml: R.curry(function(sel, html) {
+    $(sel).html(html);
+  })
+};
+
+var renderImages = R.compose(Impure.setHtml('#photos'), images);
+var app = R.compose(Impure.getJSON(renderImages), url);
+
+$(document).ready(() => {
+  $('#btSearch').click(() => {
+    var link = $('#tbSearch').val();
+    app(link);
+  });
+});
