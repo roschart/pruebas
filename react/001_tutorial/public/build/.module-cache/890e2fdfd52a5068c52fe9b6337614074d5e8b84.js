174819e25fc9211ca771786f9179a836b0d57396
roschart <jl.balirac@hotmail.com>
2022-09-08 11:19:51 +0200

    Elimar projecto

--- react/001_tutorial/public/build/.module-cache/890e2fdfd52a5068c52fe9b6337614074d5e8b84.js
+++
@@ -1,4 +0,0 @@
-React.render(
-		React.createElement("h1", null, "Hola Mundo con fichero aparte!"),
-		document.getElementById('example')
-		);
--- react/001_tutorial/public/src/helloword.js
+++
@@ -1,4 +0,0 @@
-React.render(
-		<h1>Hola Mundo con fichero aparte!</h1>,
-		document.getElementById('example')
-		);
