d5d92e887c7bfd82535bbece89a6324577fce93d
roschart <jl.balirac@hotmail.com>
2022-09-08 11:12:33 +0200

    remove project without README

--- react/react-skeleton/todelete.js
+++
@@ -1,7 +0,0 @@
-var React= require('react');
-var component = require('./public/js/components.js');
-
-var app=React.createFactory(component);
-console.log(app);
-var reactHml=React.renderToString(app({}));
-console.log(reactHml);
